24724270e972b2fec67f4fc43bf95b81e62e1a29
JasonBBelcher <belcher.jason@gmail.com>
2020-05-10 15:15:02 -0400

    test if it works

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import { Factory } from "./Engine/Config/Registry/Factory";
 import * as registry from "./Engine/Config/Registry/RegisteredClasses";
 
-// // load the map into player navigation.
-// const player = Factory.construct(registry.default, "Player", "FIGHTER");
+// load the map into player navigation.
+const player = Factory.construct(registry.default, "Player", "FIGHTER");
 
-// player.move('east');
-// console.log(player.checkIfMonstersToFight())
-// // // check that a player can't move once a fight has started
+player.move('east');
+console.log(player.checkIfMonstersToFight())
+// // check that a player can't move once a fight has started
 
-// const potion = Factory.construct(registry.default, "Item", 'STRENGTH_POTION');
-// const potion2 = Factory.construct(registry.default, "Item", 'STRENGTH_POTION');
+const potion = Factory.construct(registry.default, "Item", 'STRENGTH_POTION');
+const potion2 = Factory.construct(registry.default, "Item", 'STRENGTH_POTION');
 
-// console.log(player.placeInInventory(potion));
-// player.placeInInventory(potion2).useItemFromInventory('STRENGTH_POTION');
+console.log(player.placeInInventory(potion));
+player.placeInInventory(potion2).useItemFromInventory('STRENGTH_POTION');
 
 
 // testing monster factory for populating loot to drop when monster is killed.
-const monster = Factory.construct(registry.default, "Monster", "LOWER_GOBLIN_SCOUT")
-const monster2 = Factory.construct(registry.default, "Monster", "GOBLIN_WARRIOR")
+// const monster = Factory.construct(registry.default, "Monster", "LOWER_GOBLIN_SCOUT")
+// const monster2 = Factory.construct(registry.default, "Monster", "GOBLIN_WARRIOR")
 
-console.log(monster);
-console.log(monster2);
+// console.log(monster);
+// console.log(monster2);
 
